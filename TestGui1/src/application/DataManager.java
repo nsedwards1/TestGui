e9a1f51d97d8e43fae2f1823b47f758e91a92fc5
Nick Edwards <nsedwards@gmail.com>
2014-04-29 21:50:26 -0600

    somewhat stable version before added tabs and stuff

--- TestGui1/src/application/DataManager.java
+++ TestGui1/src/application/DataManager.java
@@ -8,6 +8,8 @@ public class DataManager {
 
 	private static HashMap<String, LinkedList<Boolean>> booleanMap = new HashMap<String, LinkedList<Boolean>>();
 	private static HashMap<String, LinkedList<Double>> doubleMap = new HashMap<String, LinkedList<Double>>();
+	private static HashMap<String, LinkedList<Float>> floatMap = new HashMap<String, LinkedList<Float>>();
+	private static HashMap<String, LinkedList<Float>> boolfloatMap = new HashMap<String, LinkedList<Float>>();
 	
 	private static Random random = new Random();
 	
@@ -19,28 +21,48 @@ public class DataManager {
 		return doubleMap.get(name);
 	}
 	
+	public static synchronized LinkedList<Float> getFloatList(String name) {
+		return floatMap.get(name);
+	}
+	public static synchronized LinkedList<Float> getBoolFloatList(String name) {
+		return boolfloatMap.get(name);
+	}
+	
 	
 	public static synchronized void initDummyData(int numberOfSamples) {
 		for(int i = 0; i < 10; i++) {
 			String bName = "bool" + i;
 			String dName = "double" + i;
+			String fName = "float" + i;
+			String bfName = "boolfloat" + i;
 			LinkedList<Boolean> bList = new LinkedList<Boolean>();
 			LinkedList<Double> dList = new LinkedList<Double>();
+			LinkedList<Float> fList = new LinkedList<Float>();
+			LinkedList<Float> bfList = new LinkedList<Float>();
 			
 			bList.add(false);
 			
 			for(int j = 1; j < numberOfSamples; j++) {
 				double val = random.nextDouble();
 				if(val > 0.98)
+				{
 					bList.add(!bList.getLast());		// 2% chance of a change
+					bfList.add(1f);
+				}
 				else
+				{
 					bList.add(bList.getLast());			// 98% chance to stay the same
+					bfList.add(0f);
+				}
 				
 				dList.add(val * 60);			// Value between 0 and 60
+				fList.add((float) (val * 60));			// Value between 0 and 60
 			}
 			
 			booleanMap.put(bName, bList);
 			doubleMap.put(dName, dList);
+			floatMap.put(fName, fList);
+			boolfloatMap.put(bfName, bfList);
 		}
 	}
 	
@@ -48,22 +70,34 @@ public class DataManager {
 		for(int i = 0; i < 10; i++) {
 			String bName = "bool" + i;
 			String dName = "double" + i;
+			String fName = "float" + i;
+			String bfName = "boolfloat" + i;
 			LinkedList<Boolean> bList = booleanMap.get(bName);
 			LinkedList<Double> dList = doubleMap.get(dName);
+			LinkedList<Float> fList = floatMap.get(fName);
+			LinkedList<Float> bfList = boolfloatMap.get(bfName);
 			
 			bList.add(false);
 			
 			for(int j = 0; j < 10; j++) {
 				double val = random.nextDouble();
 				if(val > 0.98)
+				{
 					bList.add(!bList.getLast());		// 2% chance of a change
+					bfList.add(1f);
+				}
 				else
+				{
 					bList.add(bList.getLast());			// 98% chance to stay the same
-				
+					bfList.add(0f);
+				}
 				dList.add(val * 60);			// Value between 0 and 60
+				fList.add((float) (val * 60));			// Value between 0 and 60
 				
 				bList.removeFirst();
 				dList.removeFirst();
+				fList.removeFirst();
+				bfList.removeFirst();
 			}
 			
 		}
