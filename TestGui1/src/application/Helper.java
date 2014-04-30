647e07e7691369c6ed52709dbfe047075e7d2b90
Nick Edwards <nsedwards@gmail.com>
2014-04-30 13:13:49 -0600

    somewhat better stable point before adding tabs and stuff

--- TestGui1/src/application/Helper.java
+++ TestGui1/src/application/Helper.java
@@ -10,48 +10,11 @@ import javafx.scene.chart.XYChart;
 
 public class Helper {
 	
-	public void addBoolToChart (LinkedList<Boolean> ll, 
-								LineChart<Double, Double> lineChart)
-	{
-		ObservableList<XYChart.Series<Double, Double>> boolChartData = FXCollections.observableArrayList();
-		
-		LineChart.Series<Double, Double> boolSeries = new LineChart.Series<Double, Double>();
-        boolSeries.setName("Bool Series 1");
-		double i=0;
-		for (boolean b : ll)
-		{
-			boolSeries.getData().add(new XYChart.Data<Double, Double>(i, (b) ? 1.0 : 0.0));
-			i++;
-		}
-		boolChartData.add(boolSeries);
-		lineChart.setCreateSymbols(false);
-		lineChart.setData(boolChartData);
-	}
-	
-	//ObservableList<XYChart.Series<Double, Double>> lineChartData2 = FXCollections.observableArrayList();
-	public void addDoubleToChart (LinkedList<Double> ll, 
-								  LineChart<Double, Double> lineChart)
-	{
-		ObservableList<XYChart.Series<Double, Double>> doubleChartData = FXCollections.observableArrayList();
-		
-		LineChart.Series<Double, Double> doubleSeries = new LineChart.Series<Double, Double>();
-        doubleSeries.setName("Double Series 1");
-		double i=0;
-		for (double d : ll)
-		{
-			doubleSeries.getData().add(new XYChart.Data<Double, Double>(i, d));
-			i++;
-		}
-		doubleChartData.add(doubleSeries);
-		lineChart.setCreateSymbols(false);
-		lineChart.setData(doubleChartData);
-	}
-	
-	public ObservableList<XYChart.Series<Float, Float>> floatChartData = FXCollections.observableArrayList();
-	public void addFloatToChart(LinkedList<Float> valueList, 
+	//Add a single Linked List to the chart
+	public void assignListToChart(LinkedList<Float> valueList, 
 			  					LineChart<Float, Float> lineChart)
 	{
-		//ObservableList<XYChart.Series<Float, Float>> floatChartData = FXCollections.observableArrayList();
+		ObservableList<XYChart.Series<Float, Float>> floatChartData = FXCollections.observableArrayList();
 		
 		LineChart.Series<Float, Float> floatSeries = new LineChart.Series<Float, Float>();
         //floatSeries.setName("Float Series 1");
@@ -66,10 +29,11 @@ public class Helper {
 		lineChart.setData(floatChartData);
 	}
 
-	public void addFloatToChart(List<LinkedList<Float>> valueListArray, 
+	//Add a single Linked List to the chart
+	public void assignListToChart(List<LinkedList<Float>> valueListArray, 
 				LineChart<Float, Float> lineChart)
 	{
-		ObservableList<XYChart.Series<Float, Float>> lineChartData3 = FXCollections.observableArrayList();
+		ObservableList<XYChart.Series<Float, Float>> floatChartData = FXCollections.observableArrayList();
 		
 		LineChart.Series<Float, Float> floatSeries = new LineChart.Series<Float, Float>();
 				
@@ -81,12 +45,29 @@ public class Helper {
 				floatSeries.getData().add(new XYChart.Data<Float, Float>(i, f));
 				i++;
 			}
-			lineChartData3.add(floatSeries);
+			floatChartData.add(floatSeries);
 			floatSeries = new LineChart.Series<Float, Float>();
         }
-		//lineChartData3.add(floatSeries);
+		//floatChartData.add(floatSeries);
 		lineChart.setCreateSymbols(false);
-		lineChart.setData(lineChartData3);
+		lineChart.setData(floatChartData);
+	}
+	
+	public ObservableList<XYChart.Series<Float, Float>> getObsList (LinkedList<Float> valueList)
+	{
+		ObservableList<XYChart.Series<Float, Float>> floatChartData = FXCollections.observableArrayList();
+		
+		LineChart.Series<Float, Float> floatSeries = new LineChart.Series<Float, Float>();
+        //floatSeries.setName("Float Series 1");
+		float i=0;
+		for (float f : valueList)
+		{
+			floatSeries.getData().add(new XYChart.Data<Float, Float>(i, f));
+			i++;
+		}
+		floatChartData.add(floatSeries);
+		
+		return floatChartData;
 	}
 	
 }
